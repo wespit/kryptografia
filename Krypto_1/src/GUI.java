1e7e97455c07e0f70e9898d1708adf35b9782f1c
pawel musial <musial.pawel@o2.pl>
2011-03-04 20:46:21 +0100

    zadanie, przydaloby sie potestowac jeszcze

---
+++ Krypto_1/src/GUI.java
@@ -0,0 +1,344 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import javax.swing.ButtonGroup;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+
+
+
+public class GUI {
+
+	//
+	JFrame frame;
+	JButton doIt;
+	OnetimePad otp;
+	JTextField key;
+	JLabel keyl;
+	File outFilef;
+	
+	
+	//input part
+	JTextArea in;
+	JFileChooser inFile;
+	JLabel inFileName;
+	JButton inFileChoose;
+	//output part
+	JTextArea out;
+	JFileChooser outFile;
+	JLabel outFileName;
+	JButton outFileChoose;
+	
+	//configuration part
+	ButtonGroup ed; //button group for encryption/decryption switching
+	JLabel encDec;
+	JLabel binASCII;
+	ButtonGroup ba; //button group for binary/ascii mode switching
+	JRadioButton enc, dec;
+	JRadioButton bin, ascii;
+	
+	
+	void initGUI () {
+		
+		doIt = new JButton("Wykonaj.");
+		key = new JTextField();
+		key.setSize(new Dimension(200,20 ));
+		key.setMaximumSize(new Dimension(200,20 ));
+		keyl = new JLabel("Klucz:");
+		
+		inFile = new JFileChooser();
+		outFile = new JFileChooser();
+		in = new JTextArea();
+		in.setLineWrap(true);
+		in.setAutoscrolls(true);
+		in.setSize(100, 100);
+		in.setMinimumSize(in.getSize());
+		out = new JTextArea();
+		out.setLineWrap(true);
+		out.setAutoscrolls(true);
+		out.setSize(100,100);
+		out.setMinimumSize(out.getSize());
+		
+		inFileName = new JLabel("no file");
+		inFileName.setMinimumSize(new Dimension(100,20));
+		outFileName = new JLabel("no file");
+		outFileName.setMinimumSize(new Dimension(100,20));
+		
+		encDec = new JLabel("Co chcesz zrobic? ");
+		binASCII = new JLabel("Sposób odczytu/zapisu: ");
+		
+		ed = new ButtonGroup();
+		enc = new JRadioButton("zaszyfruj");
+		enc.setSelected(true);
+		dec = new JRadioButton("odszyfruj");
+		ed.add(enc);
+		ed.add(dec);
+		
+		ba = new ButtonGroup();
+		bin = new JRadioButton("BIN");
+		ascii = new JRadioButton("ASCII");
+		ascii.setSelected(true);
+		ba.add(bin);
+		ba.add(ascii);
+		
+		inFileChoose = new JButton("wejście");
+		outFileChoose = new JButton("wyjście");
+		
+		otp = new OnetimePad();
+		otp.setMode(EncryptionMode.ASCII);
+		frame = new JFrame();
+		
+		JScrollPane outp = new JScrollPane(out);
+		JScrollPane inp = new JScrollPane(in);
+		GroupLayout l = new GroupLayout(frame.getContentPane());
+		frame.getContentPane().setLayout(l);
+		l.setAutoCreateGaps(true);
+		l.setAutoCreateContainerGaps(true);
+		l.setHorizontalGroup(l.createSequentialGroup()
+				///options
+				.addGroup(l.createParallelGroup()
+					.addComponent(encDec)
+					.addComponent(enc)
+					.addComponent(dec)
+					.addComponent(binASCII)
+					.addComponent(bin)
+					.addComponent(ascii)
+				)
+				///in
+				.addGroup(l.createParallelGroup()
+					.addComponent(inp)
+					.addComponent(inFileName)
+					.addComponent(inFileChoose)
+				)
+				///out
+				.addGroup(l.createParallelGroup()
+					.addComponent(outp)
+					.addComponent(outFileName)
+					.addComponent(outFileChoose)
+				)
+				.addGroup(l.createParallelGroup()
+					.addComponent(keyl)
+					.addComponent(key)
+					.addComponent(doIt)
+				)
+				);
+		
+		l.setVerticalGroup(l.createParallelGroup()
+				///options
+				.addGroup(l.createSequentialGroup()
+					.addComponent(encDec)
+					.addComponent(enc)
+					.addComponent(dec)
+					.addComponent(binASCII)
+					.addComponent(bin)
+					.addComponent(ascii)
+				)
+				///in
+				.addGroup(l.createSequentialGroup()
+					.addComponent(inp)
+					.addComponent(inFileName)
+					.addComponent(inFileChoose)
+				)
+				///out
+				.addGroup(l.createSequentialGroup()
+					.addComponent(outp)
+					.addComponent(outFileName)
+					.addComponent(outFileChoose)
+				)
+				.addGroup(l.createSequentialGroup()
+					.addComponent(keyl)
+					.addComponent(key)
+					.addComponent(doIt)
+				)
+				);
+		
+		
+		frame.pack();
+		frame.setSize(700, 300);
+		frame.setMinimumSize(new Dimension(550, 200));
+
+		
+	}
+	
+	void setActions() {
+		
+		//mode selection
+		bin.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (((JRadioButton)(e.getSource())).isOpaque()) {
+				otp.setMode(EncryptionMode.Binary);	
+				}
+			}
+		});
+		ascii.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if ( ((JRadioButton)(e.getSource())).isOpaque()) {
+					otp.setMode(EncryptionMode.ASCII);
+				}
+			}
+		});
+		
+		
+		
+		//File selection
+		inFileChoose.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if ( ((JButton)(e.getSource())).isOpaque()) {
+					//show open dialog
+					inFile.showOpenDialog(null);
+					//get data from file
+//					otp.getDataFromFile(inFile.getSelectedFile());
+					//set in TextArea
+//					if ( otp.getMode() == EncryptionMode.ASCII) {
+//						in.setText(otp.getAsciiData());
+//					} else {
+//						in.setText(otp.bitsetToString(otp.getBinaryData()));
+//					}
+					//set Filename label
+					inFileName.setText(inFile.getSelectedFile().getName());
+				}
+			}
+		});
+		
+		outFileChoose.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if ( ((JButton)(e.getSource())).isOpaque()) {
+					//show open dialog
+					outFile.showSaveDialog(null);
+					outFile.setVisible(true);
+					outFilef = outFile.getSelectedFile();		
+					outFileName.setText(outFile.getSelectedFile().getName());
+				}
+			}
+		});
+		
+		
+		
+		doIt.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (((JButton)e.getSource()).isOpaque()) {
+					
+					if ( !key.getText().isEmpty()) {
+						if (inFile.getSelectedFile() != null) {
+							otp.getDataFromFile(inFile.getSelectedFile());
+							otp.setMode(ascii.isSelected()?EncryptionMode.ASCII:EncryptionMode.Binary);
+							in.setText(otp.getMode()==
+								EncryptionMode.ASCII?
+									otp.getAsciiData():
+									otp.bitsetToString(otp.getBinaryData()));
+						} else {
+//						if (otp.getAsciiData().isEmpty() && otp.getBinaryData().isEmpty()) {
+							if (!in.getText().isEmpty())
+								otp.getDataFromGUI(in.getText());
+							else {
+								JOptionPane.showMessageDialog(frame, "Nie podano danych wejściowych");
+								return;
+							}
+						}								
+//						else {
+						//do it properly! :D
+						try {
+							if (enc.isSelected()) {
+								otp.EncryptData(key.getText());
+								//save result
+								if (otp.getMode() == EncryptionMode.ASCII) {
+									out.setText(otp.getAsciiEncoded());
+									OutputStreamWriter o= new OutputStreamWriter(new FileOutputStream(outFilef));
+									o.write(out.getText());
+									o.close();
+								} else {
+									out.setText(otp.bitsetToString(otp.getBinaryEncoded()));
+									OutputStreamWriter o= new OutputStreamWriter(new FileOutputStream(outFilef));
+									o.write(otp.binaryToString(otp.getBinaryEncoded()));
+									o.close();
+								}
+							} else if (dec.isSelected()) {
+								otp.DecryptData(key.getText());
+								//save result
+								if (otp.getMode() == EncryptionMode.ASCII) {
+									out.setText(otp.getAsciiDecoded());
+									OutputStreamWriter o= new OutputStreamWriter(new FileOutputStream(outFilef));
+									o.write(out.getText());
+									o.close();
+								} else {
+									out.setText(otp.bitsetToString(otp.getBinaryDecoded()));
+									OutputStreamWriter o= new OutputStreamWriter(new FileOutputStream(outFilef));
+									o.write(otp.binaryToString(otp.getBinaryDecoded()));
+									o.close();
+								}
+							}
+						} catch (Exception e1) {
+							e1.printStackTrace();
+							JOptionPane.showMessageDialog(frame, "Nie podano sciezki pliku do zapisu");
+						}
+						
+					}	else {
+						JOptionPane.showMessageDialog(frame,"Nie podano klucza.\n");	
+					}
+					otp = null;
+					otp= new OnetimePad();
+					inFile = new JFileChooser();
+					outFile = new JFileChooser();
+					otp.setMode((ascii.isSelected()?EncryptionMode.ASCII:EncryptionMode.Binary));
+				}
+
+			}
+		});
+		
+		
+		frame.addWindowListener(new WindowListener() {
+			
+			public void windowOpened(WindowEvent e) {}
+			public void windowIconified(WindowEvent e) {}
+			public void windowDeiconified(WindowEvent e) {}
+			public void windowDeactivated(WindowEvent e) {}
+			public void windowActivated(WindowEvent e) {}
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			@Override
+			public void windowClosed(WindowEvent e) {
+				System.exit(0);			
+			}
+		});
+	}
+	
+	
+	public GUI() {
+		initGUI();
+		setActions();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		new GUI().frame.setVisible(true);
+
+	}
+
+}
---
+++ Krypto_1/src/OnetimePad.java
@@ -0,0 +1,240 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.BitSet;
+
+
+public class OnetimePad {
+	
+
+	
+	BitSet binaryData, binaryEncoded, binaryDecoded, binaryKey;
+	String asciiData, asciiEncoded, asciiDecoded, asciiKey;
+	EncryptionMode mode;
+	final char [] ASCIItable = new char[256];
+	
+	public OnetimePad() {
+		asciiData = new String();
+		binaryData = new BitSet();
+		for (int i = 0; i < ASCIItable.length; i++) {
+			ASCIItable[i]=(char)i;
+//			System.out.print(ASCIItable[i]);
+		}
+
+	}
+	
+	private BitSet encryptBin () {
+		BitSet binaryEnc = new BitSet(binaryData.size());
+		for ( int i=0; i< binaryData.size(); i++) {
+			if (binaryData.get(i))
+				binaryEnc.set(i);
+		}
+		binaryEnc.xor(binaryKey);
+		return binaryEnc;
+	}
+	private String encryptASCII () {
+		String asciiEnc = new String();
+		for (int i=0; i<asciiData.length(); i++) {
+			int asd = asciiData.charAt(i);
+			int ask = asciiKey.charAt(i);
+			asciiEnc=asciiEnc+(char)((asd+ask)%ASCIItable.length);
+		}
+		return asciiEnc;
+	}
+	private BitSet decryptBin() {
+		BitSet binaryDec = new BitSet(binaryData.size());
+		for ( int i=0; i< binaryData.size(); i++) {
+			if (binaryData.get(i))
+				binaryDec.set(i);
+		}
+		binaryDec.xor(binaryKey);
+		return binaryDec;
+	}
+	private String decryptASCII() {
+		String asciiDec=new String();
+		for (int i=0; i<asciiData.length(); i++) {
+			int asd = asciiData.charAt(i);
+			int ask = asciiKey.charAt(i);
+			
+			int modsub = asd-ask;
+			if ( modsub < 0)
+				modsub=(modsub+ASCIItable.length)%ASCIItable.length;
+			else
+				modsub = modsub%ASCIItable.length;
+			asciiDec=asciiDec+(char)(modsub);
+		}
+		return asciiDec;
+	}
+	
+	
+	void getDataFromFile(File f) {
+		try {
+			if (mode == EncryptionMode.ASCII) {
+				BufferedReader in = new BufferedReader(new FileReader(f));
+				String line = new String();
+				while ( (line=in.readLine()) != null) {
+					asciiData=asciiData+line+'\n';
+				}
+				in.close();
+				System.out.print(asciiData);
+			} else {
+				FileInputStream in = new FileInputStream(f);
+				int n = in.available();
+				byte [] arr = new byte[n];
+				in.read(arr);
+				binaryData = new BitSet(arr.length*8);
+				for (int i = 0; i < arr.length; i++) {
+					for ( int j=0; j<8; j++) {
+						if ((((arr[i] >> j) & 1) == 1)) {
+							binaryData.set(i*8+j);
+						}
+					}
+				}
+				in.close();
+			}
+		} catch (FileNotFoundException e) {e.printStackTrace();
+		} catch (IOException e) {e.printStackTrace();}
+	}
+	
+	void getDataFromGUI (String data) {
+		if (mode == EncryptionMode.ASCII) {
+			asciiData =""+data;
+		} else {
+			binaryData = new BitSet(data.length());
+//			char [] arr = data.toCharArray();
+			for (int i = 0; i < data.length(); i++) {
+				if (data.charAt(i) == '1')
+					binaryData.set(i);
+//				byte b = (byte)arr[i];
+//				for ( int j=0; j<9; j++) {
+//					if ((((b >> j) & 1) == 1)) {
+//						binaryData.set(i*8+j);
+//					}
+//				}
+			}
+		}
+	}
+	
+	void EncryptData(String key) {
+		if (mode == EncryptionMode.ASCII) {
+			String keytmp = new String();
+			keytmp=""+key;
+			while ( keytmp.length() < asciiData.length()) {
+				keytmp=keytmp+key;
+			}
+			asciiKey = keytmp;
+			asciiEncoded = encryptASCII();
+			
+		} else {
+			String keytmp = new String();
+			keytmp=""+key;
+			while (keytmp.length()*8 < binaryData.length() ) {
+				keytmp=keytmp+key;
+			}
+			binaryKey = new BitSet(keytmp.length()*8);
+			for ( int i=0; i<keytmp.length(); i++) {
+				byte b =(byte) keytmp.charAt(i);
+				for ( int j=0; j<8; j++) {
+					if ((((b >> j) & 1) == 1)) {
+						binaryKey.set(i*8+j);
+					}
+				}
+			}
+			//System.out.println("BINARYKEY=="+bitsetToString(binaryKey));
+			binaryEncoded = encryptBin();
+		}
+		
+	}
+	
+	void DecryptData(String key) {
+		if (mode == EncryptionMode.ASCII) {
+			String keytmp = new String();
+			keytmp=""+key;
+			while ( keytmp.length() < asciiData.length()) {
+				keytmp=keytmp+key;
+			}
+			asciiKey = keytmp;
+			asciiDecoded = decryptASCII();
+		} else {
+			String keytmp = new String();
+			keytmp=""+key;
+			while ( keytmp.length()*8 < binaryData.length()) {
+				keytmp=keytmp+key;
+			}
+			binaryKey = new BitSet(keytmp.length()*8);
+			for ( int i=0; i<keytmp.length(); i++) {
+				byte b =(byte) keytmp.charAt(i);
+				for ( int j=0; j<8; j++) {
+					if ((((b >> j) & 1) == 1)) {
+						binaryKey.set(i*8+j);
+					}
+				}
+			}
+			
+			binaryDecoded = decryptBin();
+		}
+	}
+	public BitSet getBinaryData() {
+		return binaryData;
+	}
+	public void setBinaryData(BitSet binaryData) {
+		this.binaryData = binaryData;
+	}
+	public String getAsciiData() {
+		return asciiData;
+	}
+	public void setAcsiiData(String acsiiData) {
+		this.asciiData = acsiiData;
+	}
+	public EncryptionMode getMode() {
+		return mode;
+	}
+
+	public BitSet getBinaryEncoded() {
+		return binaryEncoded;
+	}
+	public BitSet getBinaryDecoded() {
+		return binaryDecoded;
+	}
+	public String getAsciiEncoded() {
+		return asciiEncoded;
+	}
+	public String getAsciiDecoded() {
+		return asciiDecoded;
+	}
+	public void setMode(EncryptionMode binary) {
+		this.mode = binary;
+		
+	}
+	
+	
+	public String binaryToString( BitSet bs) {
+		
+		String result = new String();
+		for ( int i=0; i<bs.size(); i+=8) {
+			int c=0;
+			int max =8;
+			if (i+8 > bs.size()) max = bs.size()-i;
+			for ( byte j=0; j<max; j++) {
+				if (bs.get(i+j)) {
+				c= c + (1<<j);
+				}
+			}
+			result=result+(char)c;
+		}
+		return result;
+	}
+	public String bitsetToString(BitSet bs) {
+		String result= new String("");
+		
+		for ( int i=0;i<bs.size(); i++ ) {
+			result+=bs.get(i)?'1':'0';
+		}
+		
+		return result;
+	}
+	
+}
